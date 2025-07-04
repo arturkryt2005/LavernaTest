e21654dc90c5036e32c2b2b80f716911fd725cfb
Артур <125348508+arturkryt2005@users.noreply.github.com>
2025-07-04 15:50:31 +0300

    NotClearCode

---
+++ WpfApp13/EditWin.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace WpfApp13
+{
+    /// <summary>
+    /// Логика взаимодействия для EditWin.xaml
+    /// </summary>
+    public partial class EditWin : Window
+    {
+        private readonly OfficeEntities db = new OfficeEntities();
+        private Equipment currentEquip;
+        public EditWin(Equipment equip)
+        {
+            InitializeComponent();
+            EquioTypeComboBox.ItemsSource = db.Type.ToList();
+            StatusTypeComboBox.ItemsSource = db.Status.ToList();
+            if (equip != null)
+            {
+                currentEquip = db.Equipment.FirstOrDefault(m => m.Id == equip.Id);
+                if (currentEquip != null)
+                {
+                    NameTextBox.Text = currentEquip.Name;
+                    EquioTypeComboBox.SelectedItem = currentEquip.Type;     
+                    StatusTypeComboBox.SelectedItem = currentEquip.Status;
+                }
+            }
+        }
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            MessageBox.Show("Вы вернулись на страницу.");
+            Close();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы вышли из программы.");
+            Close();
+        }
+
+        /// <summary>
+        /// Валидация данных и сохранение материала в базе данных.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameTextBox.Text))
+            {
+                MessageBox.Show("Ошибка! Единица измерения обязательна к заполнению.");
+                return;
+            }
+
+            var selectedType = EquioTypeComboBox.SelectedItem as Type;
+            if (selectedType == null)
+            {
+                MessageBox.Show("Ошибка! Выберите тип материала из списка.");
+                return;
+            }
+
+            var selectedStatus = StatusTypeComboBox.SelectedItem as Status;
+            if (selectedType == null)
+            {
+                MessageBox.Show("Ошибка! Выберите тип статуса из списка.");
+                return;
+            }
+
+            currentEquip.Name = NameTextBox.Text;
+            currentEquip.Type = selectedType;
+            currentEquip.TypeId = selectedType.Id;
+
+            currentEquip.Status = selectedStatus;
+            currentEquip.StatusId = selectedStatus.Id;
+            try
+            {
+                db.SaveChanges();
+                MessageBox.Show("Отлично! Данные успешно сохранены");
+                new MainWindow().Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Ошибка сохранения: {ex.Message}");
+            }
+        }
+    }
+}
--- WpfApp13/MainWindow.xaml.cs
+++ WpfApp13/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
+﻿using System.Windows;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Media.Media3D;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp13
 {
@@ -20,9 +12,67 @@ namespace WpfApp13
     /// </summary>
     public partial class MainWindow : Window
     {
+        OfficeEntities db = new OfficeEntities();
+        private List<Equipment> eqipList;
+
         public MainWindow()
         {
             InitializeComponent();
+            eqipList = db.Equipment
+                .Include(e => e.Type)
+                .Include(e => e.Status)
+                .ToList();
+            vivod.ItemsSource = eqipList.Select(p => new
+            {
+                p.TypeId,
+                p.Name,
+                Status = p.Status?.Name,
+                Type = p.Type?.Name
+            }).ToList();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            new EditWin(null).Show();
+            MessageBox.Show("Вы перешли на окно добавления. Для добавления нового материала, пожалуйста, заполните все поля в форме редактирования.");
+            Close();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы вышли из системы.");
+            Close();
+        }
+
+        private void DelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            dynamic selected = vivod.SelectedItem;
+            if (selected == null)
+            {
+                MessageBox.Show("Ошибка! Выберите материал для удаления!");
+                return;
+            }
+
+            int equipId = selected.Id;
+
+            Equipment equipToDelete = db.Equipment.Find(equipId);
+
+            if (equipToDelete != null)
+            {
+                db.Equipment.Remove(equipToDelete);
+                db.SaveChanges();
+                MessageBox.Show("Оборудование успешно удален!");
+            }
+        }
+
+        private void Vivod_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (vivod.SelectedIndex >= 0 && vivod.SelectedIndex < eqipList.Count)
+            {
+                Equipment selectedEquipment = eqipList[vivod.SelectedIndex];
+                new EditWin(selectedEquipment).Show();
+                Close();
+            }
         }
     }
 }
